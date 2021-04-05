79c592c5f9dc0f1511684f8d2e7c0fb90a33e8aa
Giovani Chaves <giovanichaves@gmail.com>
2021-04-05 17:59:58 +0200

    Added swagger + api response docs

--- src/main/java/com/n26/transactions/TransactionsController.java
+++ src/main/java/com/n26/transactions/TransactionsController.java
@@ -1,11 +1,9 @@
 package com.n26.transactions;
 
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TransactionsController {
@@ -17,22 +15,20 @@ public class TransactionsController {
     }
 
     @PostMapping("/transactions")
-    public ResponseEntity postTransactions(@RequestBody TransactionDTO transactionDTO) {
-
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Success!"),
+            @ApiResponse(code = 204, message = "Transaction is older than 60 seconds"),
+            @ApiResponse(code = 400, message = "JSON is invalid"),
+            @ApiResponse(code = 422, message = "Fields are not parsable or the transaction date is in the future")
+    })
+    public void postTransactions(@RequestBody TransactionDTO transactionDTO) {
         transactionsService.registerTransaction(transactionDTO.getAmount(), transactionDTO.getTimestamp());
-
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .build();
     }
 
     @DeleteMapping("/transactions")
-    public ResponseEntity deleteTransactions() {
-
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTransactions() {
         transactionsService.deleteTransactions();
-
-        return ResponseEntity
-                .status(HttpStatus.NO_CONTENT)
-                .build();
     }
 }
